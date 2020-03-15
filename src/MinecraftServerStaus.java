d66713df50443f7c2693b72985a103237b452aed
Anzola Fabio <anzola.fabio@gmail.com>
2020-03-15 17:27:55 +0100

    Parsing json data into array works

--- src/MinecraftServerStaus.java
+++ src/MinecraftServerStaus.java
@@ -1,7 +1,6 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,6 +21,12 @@ public class MinecraftServerStaus {
      * Stores the path in which the program should store the files
      */
     private Path workingPath;
+
+    /**
+     * Stores all of the data from the server
+     */
+    private String[] data = new String[12];
+    //state, ip, port, ping, query, srv, query mismatch, motd animated, motd, nr of players, playernames, version
 
     /**
      * Constructor (String serverAddress, String folderPath)
@@ -60,6 +65,66 @@ public class MinecraftServerStaus {
             int bytesRead;
             while ((bytesRead = in.read(buffer, 0, buffer.length)) > 0) {
                 out.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
+    public void parseData () throws IOException {
+        try (
+                BufferedReader in = Files.newBufferedReader(Paths.get(workingPath + "/download.json"), StandardCharsets.UTF_8);
+
+        ) {
+            String line;
+            String allData = "";
+
+            while ((line = in.readLine()) != null) {
+                allData += line;
+            }
+
+            allData = allData.replaceAll("\\{", "");
+            allData = allData.replaceAll("}", "");
+            String[] dataTest = allData.split(",");
+
+            if (dataTest[0].substring(6, dataTest[0].length() - 1).length() < 8) {
+                if (dataTest[dataTest.length-1].substring(9, dataTest[dataTest.length-1].length()).equals("false")) {
+                    this.data[0] = String.valueOf(false);
+                }
+            } else {
+                //static values
+                this.data[0] = String.valueOf(true); //state
+                this.data[1] = dataTest[0].substring(6, dataTest[0].length() - 1); //ip
+                this.data[2] = dataTest[1].substring(7, dataTest[1].length()); //port
+                this.data[3] = dataTest[2].substring(15, dataTest[2].length()); //ping
+                this.data[4] = dataTest[3].substring(8, dataTest[3].length()); //query
+                this.data[5] = dataTest[4].substring(6, dataTest[4].length()); //srv
+                this.data[6] = dataTest[5].substring(16, dataTest[5].length()); //query mismatch
+                this.data[7] = dataTest[7].substring(15, dataTest[7].length()); //animated motd
+            }
+
+            //variable values
+            for (int i = 0; i < dataTest.length; i++) {
+                if (dataTest[i].contains("\"clean\":")) { //motd
+                    if (!dataTest[i+1].contains("\"html\":")) {
+                        this.data[8] = dataTest[i].substring(10, dataTest[i].length() - 1);
+                        this.data[8] += ' ' + dataTest[i+1].substring(1, dataTest[i+1].length() - 1);
+                    } else {
+                        this.data[8] = dataTest[i].substring(10, dataTest[i].length() - 2);
+                    }
+                }
+                if (dataTest[i].contains("\"players\":\"online\":")) { //nr of players
+                    this.data[9] = dataTest[i].substring(19, dataTest[i].length());
+                }
+                if (dataTest[i].contains("\"version\":")) { //version
+                    this.data[11] = dataTest[i].substring(11, dataTest[i].length() - 1);
+                }
+                if (dataTest[i].contains("\"list\":")) { //players
+                    this.data[10] = dataTest[i].substring(9, dataTest[i].length() - 1);
+                    int j = 1;
+                    while (!dataTest[i + j].contains("\"version\"")) {
+                        this.data[10] += ", " + dataTest[i + j].substring(1, dataTest[i + j].length() - 1);
+                        j++;
+                    }
+                }
             }
         }
     }
