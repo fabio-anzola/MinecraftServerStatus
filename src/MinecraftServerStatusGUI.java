470414e5fece3dcac66db347191c2b12e459ddd2
Anzola Fabio <anzola.fabio@gmail.com>
2020-03-19 22:29:58 +0100

    GUI init

---
+++ src/MinecraftServerStatusGUI.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MinecraftServerStatusGUI extends Application {
+
+    @Override
+    public void start(Stage stage) {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
